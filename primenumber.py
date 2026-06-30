173952f31a3bf997a20dd0a6653f1927ca08acea
M.tharani <mtharani0073@gmail.com>
2026-06-30 13:02:58 +0530

    Add files via upload

---
+++ primenumber.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the starting number: "))
+b=int(input("Enter the ending number: "))
+for n in range (a,b):
+    count=0
+    for i in range (2,n):
+        if n%i == 0:
+            count+=1
+    if count==0:
+        print(n, "is a prime number")
+    else:
+        print(n, "is not a prime number")
